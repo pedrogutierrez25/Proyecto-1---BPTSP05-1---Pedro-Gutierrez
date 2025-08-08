9e35e78c509df10fa61f1dd4144eed24d2712cff
Pedro Gutierrez <pedro-gutierrez@correo.unimet.edu.ve>
2025-08-07 23:14:36 -0400

    Docstring

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ def main():
       museomet.start()
 
 main()
+
+
+
+--------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------
+
+                                    PROYECTO #1 ----- DOCSTRING
+                                    ---------------------------
+
+    El siguiente programa de consola funge como un pequeno catalogo digital del Museo metropolitano de arte de Nueva York
+
+"""
